8a0f59232d23ca7ce76bcb107b06d1bc03714072
John Caudill <jcaudill86@gmail.com>
2026-09-19 14:20:15 -0400

    update dashboard.js for user persistance

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,4 @@
-const supabaseClient = supabase.createClient('https://lcyboumcokkwtaihvnox.supabase.co', 'sb_publishable_u6CrbHQYoheiiJjYvgUb4Q_vlT_zyH1')
+const supabaseClient = supabase.createClient('https://lcyboumcokkwtaihvnox.supabase.co', 'sb_publishable_u6CrbHQYoheiiJjYvgUb4Q_vlT_zyH1');
 
 (async function() {
     
