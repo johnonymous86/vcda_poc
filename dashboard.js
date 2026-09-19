71a6ad2aeca3cae367924bf678bae8eda36cc65e
John Caudill <jcaudill86@gmail.com>
2026-09-19 12:40:09 -0400

    add dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,10 @@
+const supabaseClient = supabase.createClient('https://lcyboumcokkwtaihvnox.supabase.co', 'sb_publishable_u6CrbHQYoheiiJjYvgUb4Q_vlT_zyH1')
+
+(async function() {
+    
+    const {data: {session}} = await supabaseClient.auth.getSession();
+
+    if(!session) {window.location.href = "user.html";}
+
+})();
+
